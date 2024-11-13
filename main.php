9a9120b2f46cbd038432fbd3790efd5be642692d
Gabby Prusse <gabbyprusse@u.boisestate.edu>
2024-11-13 14:55:26 -0700

    files made

---
+++ main.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>RSP & Associates</title>
+    <link rel="stylesheet" href="mystyle.css">
+
+</head>
+
+<body>
+<?php require 'header.html'?>
+<div name="intro">
+    <p>Ryan S. Prusse, MAI has been serving the Pacific Northwest since 1991 and founded RSP & Associates
+    in 1998. Ryan and his associates have extensive experience providing consultation services and appraisals
+    of commercial and other complex real property. His firm works with a wide array of lenders, attorneys,
+    accountants, public agencies, and non-profit entities. A broad geographic radius is served, primarily
+        Oregon and Washington. Feasibility studies, expert witness testimony, desk/field appraisal reviews, property tex appeals and lease-leasehold analyses are also preformed.
+    </p>
+</div>
+<?php require 'footer.html'?>;
+</body>
+</html>
